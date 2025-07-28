07548a7c6f7a302915dcf03f869775c4128f8db3
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-29 01:40:59 +0300

    (client): add 'my cargos' page

---
+++ client/src/app/kargolarim/(components)/cargo-list.tsx
@@ -0,0 +1,11 @@
+import CargoListItem from "./cargo-list-item";
+
+export default function CargoList() {
+  return (
+    <div className="space-y-2">
+      <CargoListItem />
+      <CargoListItem />
+      <CargoListItem />
+    </div>
+  );
+}
