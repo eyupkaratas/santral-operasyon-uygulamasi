73ff3a7811b1376201c2132c8028b2e4979c1379
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-29 12:54:30 +0300

    (client): add route protection

---
+++ client/src/middleware.ts
@@ -0,0 +1,38 @@
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = ["/anasayfa"];
+const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+function redirectToHome(request: NextRequest) {
+  const homeUrl = new URL("/anasayfa", request.url);
+  return NextResponse.redirect(homeUrl);
+}
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const isPublic = publicRoutes.some(
+    (route) => pathname.startsWith(route) //
+  );
+
+  if (isPublic) return NextResponse.next();
+
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    return redirectToHome(request);
+  }
+
+  try {
+    await jwtVerify(token, jwtSecret);
+  } catch {
+    return redirectToHome(request);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
