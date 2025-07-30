7235b403db64edaf5cb1585e83d85b4f00681fc4
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-30 17:36:04 +0300

    (clint): chore

--- client/src/components/profile.tsx
+++ client/src/components/profile.tsx
@@ -5,25 +5,9 @@ import { useEffect } from "react";
 import { toast } from "sonner";
 import ProfileCard from "./profile-card";
 
-type CardItemProps = {
-  label: string;
-  content: string;
-};
-
 type ProfileProps = {
   decodedToken: DecodedToken;
 };
-
-function CardItem({ label, content }: CardItemProps) {
-  return (
-    <div className="flex flex-col items-center justify-center border-b">
-      <div className="mb-2 text-center">
-        <p className="font-bold">{label}</p>
-        <p className="whitespace-pre-line">{content}</p>
-      </div>
-    </div>
-  );
-}
 
 export default function Profile({ decodedToken }: ProfileProps) {
   const { personalAdSoyad, dahiliNo, birim, unvan, eposta, rol } = decodedToken;
