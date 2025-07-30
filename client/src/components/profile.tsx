4ed38bc73836825f6b8c68dd267eb9cf6d492fb3
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-30 14:57:42 +0300

    (client): add dynamic data to profile page

--- client/src/components/profile.tsx
+++ client/src/components/profile.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DecodedToken } from "@/types/decoded-token";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 type CardItemProps = {
   label: string;
   content: string;
+};
+
+type ProfileProps = {
+  decodedToken: DecodedToken;
 };
 
 function CardItem({ label, content }: CardItemProps) {
@@ -10,26 +19,48 @@ function CardItem({ label, content }: CardItemProps) {
     <div className="flex flex-col items-center justify-center border-b">
       <div className="mb-2 text-center">
         <p className="font-bold">{label}</p>
-        <p>{content}</p>
+        <p className="whitespace-pre-line">{content}</p>
       </div>
     </div>
   );
 }
 
-export default function Profile() {
+export default function Profile({ decodedToken }: ProfileProps) {
+  const { personalAdSoyad, dahiliNo, birim, unvan, eposta, rol } = decodedToken;
+
+  useEffect(() => {
+    toast.success(
+      <>
+        <p className="text-center">
+          Hoş geldin <strong>{personalAdSoyad}</strong>.
+        </p>
+        <p className="text-center">
+          Rolün: <strong>{rol}</strong>
+        </p>
+      </>,
+      {
+        closeButton: true,
+        position: "top-center",
+        classNames: {
+          content: "w-full",
+        },
+      }
+    );
+  }, []);
+
   return (
     <Card>
       <CardHeader className="border-b-1">
         <CardTitle className="mb-4 text-center">Profil</CardTitle>
       </CardHeader>
       <CardContent className="space-y-4">
-        <CardItem label="İsim, Soyisim" content="Eyüp Karataş" />
+        <CardItem label="İsim, Soyisim" content={personalAdSoyad} />
 
-        <CardItem label="Birim, Ünvan" content="Bilgi İşlem, Yazılım Mühendisi" />
+        <CardItem label="Birim, Ünvan" content={`${birim}\n${unvan}`} />
 
-        <CardItem label="Dahili No" content="2615" />
+        <CardItem label="Dahili No" content={dahiliNo} />
 
-        <CardItem label="E-Posta" content="eyup.karatas@manisa.bsb.tr" />
+        <CardItem label="E-Posta" content={eposta} />
       </CardContent>
     </Card>
   );
