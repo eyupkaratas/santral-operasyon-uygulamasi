fcfbf1497efea6be47a48865e89d407503f28d11
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-30 20:11:54 +0300

    (client): add skeleton loadings for unit page

--- client/src/app/birimler/page.tsx
+++ client/src/app/birimler/page.tsx
@@ -1,7 +1,10 @@
 import { getPersonnelsAction } from "@/actions/personnel";
 import { getUnitsAction } from "@/actions/unit";
+import AsyncWrapper from "@/components/async-wrapper";
 import UnitFilter from "./(components)/unit-filter";
+import UnitFilterSkeleton from "./(components)/unit-filter-skeleton";
 import UnitList from "./(components)/unit-list";
+import UnitListSkeleton from "./(components)/unit-list-skeleton";
 
 export default async function Page({
   searchParams,
@@ -10,15 +13,22 @@ export default async function Page({
 }) {
   const birimId = (await searchParams).birimId?.toString();
 
-  const personnels = await getPersonnelsAction(birimId);
-  const units = await getUnitsAction();
-
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-lg space-y-2">
         <div className="text-center">Bilgi İşlem</div>
-        <UnitFilter units={units} />
-        <UnitList personnels={personnels} />
+        <AsyncWrapper
+          loader={getUnitsAction}
+          fallback={<UnitFilterSkeleton />}
+          render={(units) => <UnitFilter units={units} />}
+        />
+
+        <AsyncWrapper
+          key={birimId}
+          loader={() => getPersonnelsAction(birimId)}
+          fallback={<UnitListSkeleton />}
+          render={(personnels) => <UnitList personnels={personnels} />}
+        />
       </div>
     </div>
   );
