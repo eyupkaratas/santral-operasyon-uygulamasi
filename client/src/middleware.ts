412cde4cc4f42ef84ad183654002c463a3bd5e88
Eyüp Karataş <141742400+eyupkaratas@users.noreply.github.com>
2025-07-31 14:42:48 +0300

    (client): protect routes

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,33 +1,52 @@
 import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
+import { DecodedToken } from "./types/decoded-token";
 
 const publicRoutes = ["/anasayfa"];
+const adminRoutes = [""];
+const operatorRoutes = ["/numara-kaydi"];
+
 const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-function redirectToHome(request: NextRequest) {
+function redirectToLogin(request: NextRequest) {
   const homeUrl = new URL("/anasayfa", request.url);
+  return NextResponse.redirect(homeUrl);
+}
+
+function redirectToProfile(request: NextRequest) {
+  const homeUrl = new URL("/profil", request.url);
   return NextResponse.redirect(homeUrl);
 }
 
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  const isPublic = publicRoutes.some(
-    (route) => pathname.startsWith(route) //
-  );
+  const isPublicRoute = publicRoutes.some((route) => pathname.startsWith(route));
 
-  if (isPublic) return NextResponse.next();
+  if (isPublicRoute) return NextResponse.next();
 
   const token = request.cookies.get("token")?.value;
 
   if (!token) {
-    return redirectToHome(request);
+    return redirectToLogin(request);
   }
 
   try {
-    await jwtVerify(token, jwtSecret);
+    const { payload } = await jwtVerify(token, jwtSecret);
+    const decodedToken = payload as DecodedToken;
+
+    const isOperatorRoute = operatorRoutes.some((route) => pathname.startsWith(route));
+    const isAdminRoute = adminRoutes.some((route) => pathname.startsWith(route));
+
+    if (isOperatorRoute && !["Operator", "Admin"].includes(decodedToken.rol)) {
+      return redirectToProfile(request);
+    }
+
+    if (isAdminRoute && decodedToken.rol !== "Admin") {
+      return redirectToProfile(request);
+    }
   } catch {
-    return redirectToHome(request);
+    return redirectToLogin(request);
   }
 
   return NextResponse.next();
